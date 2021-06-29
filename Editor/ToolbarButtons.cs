666c93be4de77ababc26b80f2b038aebbf0f4317
Rodrigo Varga <varguiniano@gmail.com>
2021-06-29 15:08:12 +0200

    - First asset version.

---
+++ Editor/ToolbarButtons.cs
@@ -0,0 +1,221 @@
+﻿using System.IO;
+using BennyKok.ToolbarButtons;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using WhateverDevs.SceneManagement.Runtime.AddressableManagement;
+using WhateverDevs.SceneManagement.Runtime.SceneManagement;
+using WhateverDevs.TwoDAudio.Runtime;
+using Zenject;
+
+namespace WhateverDevs.DefaultToolBarButtons.Editor
+{
+    /// <summary>
+    /// Class to add all the buttons we want to the toolbar.
+    /// Reference to icons: https://unitylist.com/p/5c3/Unity-editor-icons.
+    /// </summary>
+    public static class ToolbarButtons
+    {
+        /// <summary>
+        /// Path to the config folder.
+        /// </summary>
+        private const string ConfigFolder = "Assets/Data/ToolbarButtons/Editor";
+
+        /// <summary>
+        /// Path to the config file.
+        /// </summary>
+        private const string ConfigPath = ConfigFolder + "/Config.asset";
+
+        /// <summary>
+        /// Cached reference to the config.
+        /// </summary>
+        private static ToolbarButtonsConfig config;
+
+        /// <summary>
+        /// Open the project folder.
+        /// </summary>
+        [ToolbarButton("Audio Mixer@2x", "Open toolbar buttons settings")]
+        [UsedImplicitly]
+        public static void ButtonsConfig()
+        {
+            CacheConfig();
+            Selection.activeObject = config;
+        }
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_PROJECTFOLDER
+        /// <summary>
+        /// Open the project folder.
+        /// </summary>
+        [ToolbarButton("Folder Icon", "Open project folder")]
+        [UsedImplicitly]
+        public static void OpenProjectFolder() => DefaultToolbarButtons.OpenFolder();
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_PACKAGEMANAGER
+        /// <summary>
+        /// Open the package manager.
+        /// </summary>
+        [ToolbarButton(iconName = "Package Manager", tooltip = "Package Manager")]
+        [UsedImplicitly]
+        public static void OpenPackageManager() => DefaultToolbarButtons.ShowPackageManager();
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_PROJECTSETTINGS
+        /// <summary>
+        /// Open either project settings or editor settings.
+        /// </summary>
+        [ToolbarButton("Settings", "Show Settings")]
+        [UsedImplicitly]
+        public static void OpenSettings() => DefaultToolbarButtons.ShowSettings();
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_PROJECTCONTEXT
+        /// <summary>
+        /// Open the project context.
+        /// </summary>
+        [ToolbarButton("d_EditCollider", "Project Context")]
+        [UsedImplicitly]
+        public static void ProjectContext()
+        {
+            CacheConfig();
+
+            Selection.activeObject =
+                AssetDatabase
+                   .LoadAssetAtPath<
+                        ProjectContext>(AssetDatabase.GetAssetPath(config.ProjectContext));
+        }
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_BUILD
+        /// <summary>
+        /// Opens the Console Pro window.
+        /// </summary>
+        // ReSharper disable once StringLiteralTypo
+        [ToolbarButton("d_SceneViewFX@2x", "Build")]
+        [UsedImplicitly]
+        public static void OpenBuild() => EditorApplication.ExecuteMenuItem("File/Build Settings...");
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_SCENEMANAGEMENT
+        /// <summary>
+        /// Open the addressable groups.
+        /// </summary>
+        [ToolbarButton("d_Profiler.NetworkOperations@2x", "Addressables")]
+        [UsedImplicitly]
+        public static void Addressables()
+        {
+            CacheConfig();
+
+            GenericMenu menu = new GenericMenu();
+
+            menu.AddItem(new GUIContent("Scene Manager"),
+                         false,
+                         () => Selection.activeObject =
+                                   AssetDatabase
+                                      .LoadAssetAtPath<
+                                           SceneManager>(AssetDatabase.GetAssetPath(config.SceneManager)));
+
+            menu.AddItem(new GUIContent("Addressables/Groups"),
+                         false,
+                         () => EditorApplication.ExecuteMenuItem("Window/Asset Management/Addressables/Groups"));
+
+            menu.AddItem(new GUIContent("Addressables/Version Dependence"),
+                         false,
+                         () => Selection.activeObject =
+                                   AssetDatabase
+                                      .LoadAssetAtPath<
+                                           AddressableVersionDependence>(AssetDatabase.GetAssetPath(config
+                                                                            .AddressableVersionDependence)));
+
+            menu.AddItem(new GUIContent("Addressables/Builder"),
+                         false,
+                         () => Selection.activeObject =
+                                   AssetDatabase
+                                      .LoadAssetAtPath<
+                                           AddressablesBuilder>(AssetDatabase
+                                                                   .GetAssetPath(config.AddressablesBuilder)));
+
+            menu.ShowAsContext();
+        }
+        #endif
+        
+        #if WHATEVERDEVS_TOOLBARBUTTONS_TWODAUDIO
+        /// <summary>
+        /// Open the project context.
+        /// </summary>
+        [ToolbarButton("d_Profiler.Audio@2x", "Audio Library")]
+        [UsedImplicitly]
+        public static void AudioLibrary()
+        {
+            CacheConfig();
+
+            Selection.activeObject =
+                AssetDatabase
+                   .LoadAssetAtPath<
+                        AudioLibrary>(AssetDatabase.GetAssetPath(config.AudioLibrary));
+        }
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_CONSOLEPRO
+        /// <summary>
+        /// Opens the Console Pro window.
+        /// </summary>
+        // ReSharper disable once StringLiteralTypo
+        [ToolbarButton("d_winbtn_win_max", "Console Pro")]
+        [UsedImplicitly]
+        public static void OpenConsolePro() => EditorApplication.ExecuteMenuItem("Window/Console Pro 3");
+        #endif
+
+        #if WHATEVERDEVS_TOOLBARBUTTONS_PLAYBUTTON
+        /// <summary>
+        /// Open the app initialization scene and then play.
+        /// </summary>
+        [ToolbarButton("UnityEditor.GameView", "Play from init")]
+        [UsedImplicitly]
+        public static void PlayFromInit()
+        {
+            if (Application.isPlaying) return;
+
+            try
+            {
+                EditorUtility.DisplayProgressBar("Loading", "Loading init scene...", .5f);
+
+                EditorSceneManager.SaveOpenScenes();
+
+                CacheConfig();
+
+                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(config.InitializationScene));
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+
+            EditorApplication.isPlaying = true;
+        }
+        #endif
+
+        #endif
+
+        /// <summary>
+        /// Cache or create the configuration reference.
+        /// </summary>
+        private static void CacheConfig()
+        {
+            if (config != null) return;
+
+            config =
+                AssetDatabase.LoadAssetAtPath<ToolbarButtonsConfig>(ConfigPath);
+
+            if (config != null) return;
+            if (!Directory.Exists(ConfigFolder)) Directory.CreateDirectory(ConfigFolder);
+
+            config = (ToolbarButtonsConfig) ScriptableObject.CreateInstance(typeof(ToolbarButtonsConfig));
+            AssetDatabase.CreateAsset(config, ConfigPath);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ Editor/ToolbarButtonsConfig.cs
@@ -0,0 +1,197 @@
+using JetBrains.Annotations;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using WhateverDevs.Core.Editor.Utils;
+using WhateverDevs.Core.Runtime.Common;
+using WhateverDevs.SceneManagement.Runtime.AddressableManagement;
+using WhateverDevs.SceneManagement.Runtime.SceneManagement;
+using WhateverDevs.TwoDAudio.Runtime;
+using Zenject;
+
+namespace WhateverDevs.DefaultToolBarButtons.Editor
+{
+    /// <summary>
+    /// Configuration manager for the toolbar buttons that will set scripting defines to enable or disable buttons.
+    /// </summary>
+    public class ToolbarButtonsConfig : LoggableScriptableObject<ToolbarButtonsConfig>
+    {
+        /// <summary>
+        /// Enable the toolbar buttons?
+        /// </summary>
+        [InfoBox("Changing this config recompiles the project, be patient :)")]
+        [OnValueChanged("OnEnableToolbarButtonsChange")]
+        public bool EnableToolbarButtons;
+
+        /// <summary>
+        /// Enable the project folder toolbar button?
+        /// </summary>
+        [FoldoutGroup("General")]
+        [OnValueChanged("OnEnableProjectFolderButton")]
+        public bool EnableProjectFolderButton;
+
+        /// <summary>
+        /// Enable the package manager toolbar button?
+        /// </summary>
+        [FoldoutGroup("General")]
+        [OnValueChanged("OnEnablePackageManagerButton")]
+        public bool EnablePackageManagerButton;
+
+        /// <summary>
+        /// Enable the project settings toolbar button?
+        /// </summary>
+        [FoldoutGroup("General")]
+        [OnValueChanged("OnEnableProjectSettingsButton")]
+        public bool EnableProjectSettingsButton;
+
+        /// <summary>
+        /// Enable the build toolbar button?
+        /// </summary>
+        [FoldoutGroup("General")]
+        [OnValueChanged("OnEnableBuildButton")]
+        public bool EnableBuildButton;
+        
+        /// <summary>
+        /// Enable the build toolbar button?
+        /// </summary>
+        [FoldoutGroup("General")]
+        [OnValueChanged("OnEnablePlayButton")]
+        public bool EnablePlayButton;
+
+        [FoldoutGroup("General")]
+        [ShowIf("EnablePlayButton")]
+        public SceneAsset InitializationScene;
+
+        /// <summary>
+        /// Enable the project context toolbar button?
+        /// </summary>
+        [FoldoutGroup("Zenject")]
+        [OnValueChanged("OnEnableProjectContextButton")]
+        public bool EnableProjectContextButton;
+
+        /// <summary>
+        /// Reference to the project context.
+        /// </summary>
+        [FoldoutGroup("Zenject")]
+        [ShowIf("EnableProjectContextButton")]
+        public ProjectContext ProjectContext;
+
+        /// <summary>
+        /// Enable the scene management toolbar button?
+        /// </summary>
+        [FoldoutGroup("Scene Management")]
+        [OnValueChanged("OnEnableSceneManagementButton")]
+        public bool EnableSceneManagementButton;
+
+        /// <summary>
+        /// Reference to the scene manager.
+        /// </summary>
+        [FoldoutGroup("Scene Management")]
+        [ShowIf("EnableSceneManagementButton")]
+        public SceneManager SceneManager;
+
+        /// <summary>
+        /// Reference to the AddressableVersionDependence file.
+        /// </summary>
+        [FoldoutGroup("Scene Management")]
+        [ShowIf("EnableSceneManagementButton")]
+        public AddressableVersionDependence AddressableVersionDependence;
+
+        /// <summary>
+        /// Reference to the addressable builder.
+        /// </summary>
+        [FoldoutGroup("Scene Management")]
+        [ShowIf("EnableSceneManagementButton")]
+        public AddressablesBuilder AddressablesBuilder;
+        
+        /// <summary>
+        /// Enable the 2D audio button?
+        /// </summary>
+        [FoldoutGroup("2D Audio")]
+        [OnValueChanged("OnEnable2DAudioButton")]
+        public bool Enable2DAudioLibraryButton;
+        
+        /// <summary>
+        /// Reference to the audio library.
+        /// </summary>
+        [FoldoutGroup("2D Audio")]
+        [ShowIf("Enable2DAudioLibraryButton")]
+        public AudioLibrary AudioLibrary;
+        
+        /// <summary>
+        /// Enable the console pro toolbar button?
+        /// </summary>
+        [FoldoutGroup("Third Party Plugins")]
+        [OnValueChanged("OnEnableConsoleProButton")]
+        public bool EnableConsoleProButton;
+
+        /// <summary>
+        /// Called when the EnableToolbarButtons property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableToolbarButtonsChange() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS", EnableToolbarButtons);
+
+        /// <summary>
+        /// Called when the EnableProjectFolderButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableProjectFolderButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_PROJECTFOLDER", EnableProjectFolderButton);
+
+        /// <summary>
+        /// Called when the EnablePackageManagerButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnablePackageManagerButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_PACKAGEMANAGER", EnablePackageManagerButton);
+
+        /// <summary>
+        /// Called when the EnableProjectSettingsButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableProjectSettingsButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_PROJECTSETTINGS", EnableProjectSettingsButton);
+
+        /// <summary>
+        /// Called when the EnableBuildButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableBuildButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_BUILD", EnableBuildButton);
+        
+        /// <summary>
+        /// Called when the EnableBuildButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnablePlayButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_PLAYBUTTON", EnablePlayButton);
+
+        /// <summary>
+        /// Called when the EnableProjectContextButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableProjectContextButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_PROJECTCONTEXT", EnableProjectContextButton);
+
+        /// <summary>
+        /// Called when the EnableSceneManagementButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableSceneManagementButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_SCENEMANAGEMENT", EnableSceneManagementButton);
+        
+        /// <summary>
+        /// Called when the Enable2DAudioButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnable2DAudioButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_TWODAUDIO", Enable2DAudioLibraryButton);
+        
+        /// <summary>
+        /// Called when the EnableConsoleProButton property is changed.
+        /// </summary>
+        [UsedImplicitly]
+        private void OnEnableConsoleProButton() =>
+            ScriptingDefines.SetDefine("WHATEVERDEVS_TOOLBARBUTTONS_CONSOLEPRO", EnableConsoleProButton);
+    }
+}
