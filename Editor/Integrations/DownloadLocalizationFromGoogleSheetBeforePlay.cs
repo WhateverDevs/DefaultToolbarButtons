6c4383f09c593f106f680f1e93145d3872ac70dc
Rodrigo Varga <varguiniano@gmail.com>
2023-09-21 12:54:59 +0200

    - Added a play hook to auto download localization before playing.

---
+++ Editor/Integrations/DownloadLocalizationFromGoogleSheetBeforePlay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using WhateverDevs.Localization.Editor;
+using WhateverDevs.Localization.Runtime;
+
+namespace WhateverDevs.DefaultToolBarButtons.Editor.Integrations
+{
+    /// <summary>
+    /// Play hook that refreshes the languages just before building.
+    /// </summary>
+    [CreateAssetMenu(menuName = "WhateverDevs/Localization/PlayHook",
+                     fileName = "DownloadLocalizationFromGoogleSheetBeforePlay")]
+    public class DownloadLocalizationFromGoogleSheetBeforePlay : PlayHook
+    {
+        /// <summary>
+        /// Url to refresh from.
+        /// </summary>
+        public string Url;
+
+        /// <summary>
+        /// Reference to the configuration file.
+        /// </summary>
+        public LocalizerConfigurationFile LocalizationConfigurationFile;
+
+        /// <summary>
+        /// Run before playing.
+        /// </summary>
+        public override void Run() =>
+            GoogleSheetLoader.LoadLanguages(Url, LocalizationConfigurationFile.ConfigurationData.LanguagePackDirectory);
+    }
+}
